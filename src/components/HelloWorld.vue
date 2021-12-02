5e4a3091da0f83df8a64faae32e3a808f2b256cd
Pavel Yakupov <pavel.yakupov@software-kit.ru>
2021-12-02 18:36:38 +0300

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,49 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <FormSchema :schema="schema" v-model="model" @submit.prevent="submit">
+    <button type="submit">Subscribe</button>
+  </FormSchema>
 </template>
 
+
+
+
 <script>
+import $RefParser from "@apidevtools/json-schema-ref-parser";
+
+  import FormSchema from '@formschema/native'
+  import schema from './sheme.json'
+
+  // console.log(schema);
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+    data: () => ({
+      schema: {},
+      model: {}
+    }),
+  created () {
+$RefParser.dereference(schema, (err, schema) => {
+  if (err) {
+    console.error(err);
   }
+  else {
+    // `schema` is just a normal JavaScript object that contains your entire JSON Schema,
+    // including referenced files, combined into a single object
+    this.schema = schema
+    console.log(this.schema)
+  }
+})
+  },
+    methods: {
+      submit () {
+        // this.model contains the valid data according your JSON Schema.
+        // You can submit your model to the server here
+      }
+    },
+   components: { FormSchema }
 }
 </script>
 
